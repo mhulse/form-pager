3c4fc3772096264ffae9b22be0ae11e474667d11
Micky Hulse <mickyhulse@gmail.com>
2016-06-25 12:16:32 -0700

    First pass

--- form-pager.js
+++ form-pager.js
@@ -1,176 +1,77 @@
-/**
- * This could use a lot more work.
- *
- * @see http://www.amitpatil.me/multi-step-form-with-progress-bar-and-validation/
- */
-
-(function($, document, window, undefined) {
+(function($, window, document, undefined) {
 	
 	'use strict';
 	
-	var $fp = $('#form-pager');
+	var NS = 'pager';
+	var console = (window.console || { log : function() {}, warn : function() {} });
+	var defaults;
+	var methods = {};
+	var defaults = {
+		target: '#form-pager',
+		progress: '#progress'
+	};
 	
-	function main() {
+	methods.init = function(options) {
 		
-		var current = 1;
-		var $document = $(document);
-		var $progress = $('#progress');
-		var $form = $fp.find('form');
-		var $nav = $fp.find('.steps-nav');
-		var $step = $fp.find('.step');
-		var $prev = $fp.find('.prev');
-		var $next = $fp.find('.next');
-		var $submit = $fp.find('.submit');
-		var top = function() { $document.scrollTop(0); };
-		// Change progress bar action:
-		var progress = function(step) {
-			var percent = parseFloat(100 / $step.length) * step;
-			percent = percent.toFixed();
-			$progress
-				.children('div')
-				.css('width', percent + '%')
-				.end()
-				.children('span')
-				.text(percent + '%');
-		};
-		// Hide buttons according to the current step:
-		var buttons = function(step) {
-			var limit = parseInt($step.length);
-			$nav.find('button').hide();
-			if (step < limit) {
-				$next.show();
-			}
-			if (step > 1) {
-				$prev.show();
-			}
-			if (step == limit) {
-				$next.hide();
-				$submit.show();
-			}
-		};
-		var go = function() {
+		return this.each(function() {
 			
-			var hash = window.location.hash;
+			var $this = $(this);
+			var data = $this.data(NS);
+			var settings = $.extend(true, {}, defaults, options, $this.data(NS + 'Options')); // Recursively merge defaults, options and data attribute options.
 			
-			if (hash) { // Fragment exists:
+			if ( ! data) {
 				
-				current = parseInt(hash.replace(/^[^0-9]+/, ''), 10);
+				$this.data(NS, {
+					init: false,
+					settings: settings,
+					target: $this,
+					current: 1,
+					progress: $(settings.progress)
+				});
+				
+				data = $this.data(NS);
 				
 			}
 			
-			//console.log('going', hash, current);
-			
-			// Init buttons and UI:
-			$step
-				.show()
-				.not(':eq(' + (current - 1) + ')')
-				.hide();
-			buttons(current);
-			progress(current);
-			$nav.show();
-			
-		};
-		
-		// Initialize validation plugin:
-		$form.validate({
-			ignore: ':not(:visible)',
-			rules: {
-				name: 'required',
-				// Just an example of how to do more advanced validation checks:
-				dob: {
-					required: true,
-					date: true
-				}
-			}
-			// If you don’t want to deal with the default error placement:
-			/*
-			errorPlacement: function(error, element) {
-				var $label = element.parent('label');
-				if ($label.length) {
-					$label.before(error);
-				} else {
-					element.before(error);
-				}
-			}
-			*/
-		});
-		
-		// Next button click action
-		$next.click(function($event){
-			
-			$event.preventDefault();
-			
-			if (current < $step.length) {
+			if ( ! data.init) {
 				
-				// Check validation:
-				if ($form.valid()) {
-					
-					$step.show();
-					
-					$step
-						.not(':eq(' + (current++) + ')')
-						.hide();
-					
-					top();
-					
-					progress(current);
-					
-				}
+				data.init = true;
+				
+				_main.call($this, data);
+				
+			} else {
+				
+				console.warn('jQuery.' + NS, 'already initialized on', this);
+				
 			}
-			
-			buttons(current);
 			
 		});
 		
-		// Back button click action:
-		$prev.click(function($event) {
-			
-			$event.preventDefault();
-			
-			if (current > 1) {
-				
-				current = current - 2;
-				
-				if (current < $step.length) {
-					
-					$step.show();
-					
-					$step
-						.not(':eq(' + (current++) + ')')
-						.hide();
-					
-					top();
-					
-					progress(current);
-					
-				}
-				
-			}
-			
-			buttons(current);
-			
-		});
-		
-		// Submit button click:
-		$submit.click(function() {
-			//alert('Submit button clicked');
-		});
-		
-		$(window).on('hashchange', go);
-		
-		go();
-		
-	}
+	};
 	
-	$(function() {
+	methods.showPage = function(pageNumber) {
 		
-		// Only begin if pager exists:
-		if ($fp.find('.steps').length) {
-			
-			main(); // Begin!
-			
+		$(this).find('.step')
+			.show()
+			.not(':eq(' + (pageNumber - 1) + ')')
+			.hide();
+		
+	};
+	
+	var _main = function(data) {
+		
+		//$.fn[NS].showPage.call(this, 1);
+		
+	};
+	
+	$.fn[NS] = function(method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if ((typeof method === 'object') || ( ! method)) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' +  method + ' does not exist on jQuery.' + NS);
 		}
-		
-	});
+	};
 	
-}(jQuery, document, window));
+}(jQuery, window, document));
